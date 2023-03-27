c6dc8f5b98a6818e793310679a0e2e2e08b47877
Sam Wilson <sam@samwilson.id.au>
2023-03-27 14:30:48 +0800

    Add eslint and stylelint, and fix some errors
    Add config for eslint and stylelint, and fix some of the errors.
    
    Bug: T333121
    Change-Id: Iad3c55fa9357e738367f12a38fcbf36e12d9f16c

--- assets/scripts/foreground.js
+++ assets/scripts/foreground.js
@@ -1,18 +1,21 @@
-jQuery(document).ready(function() {
+$( function () {
 
-  // Log errors
-  jQuery(document).foundation(function (response) {
-    if (window.console) console.log(response.errors);
-  });
-  
-  // The Echo extension puts an item in personal tools that Foreground really should have in the top menu
-  // to make this easier, we move it here and loaded earlier to speed up transform
-  jQuery("#pt-notifications").prependTo("#echo-notifications-alerts");
-  jQuery("#pt-notifications-message").prependTo("#echo-notifications-messages");
-  jQuery("#pt-notifications-alert").prependTo("#echo-notifications-alerts");
-  jQuery("#pt-notifications-notice").prependTo("#echo-notifications-notice");
+	// Log errors
+	$( document ).foundation( function ( response ) {
+		if ( window.console ) {
+			// eslint-disable-next-line no-console
+			console.error( response.errors );
+		}
+	} );
 
-  // Turn categories into labels
-  jQuery('#mw-normal-catlinks ul li a').addClass('label');
+	// The Echo extension puts an item in personal tools that Foreground really should have in the
+	// top menu to make this easier, we move it here and loaded earlier to speed up transform
+	$( '#pt-notifications' ).prependTo( '#echo-notifications-alerts' );
+	$( '#pt-notifications-message' ).prependTo( '#echo-notifications-messages' );
+	$( '#pt-notifications-alert' ).prependTo( '#echo-notifications-alerts' );
+	$( '#pt-notifications-notice' ).prependTo( '#echo-notifications-notice' );
 
-});
+	// Turn categories into labels
+	$( '#mw-normal-catlinks ul li a' ).addClass( 'label' );
+
+} );
